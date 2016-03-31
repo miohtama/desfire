dde96c2c6489e8bf9036a73b2300aee4f79f3138
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-03-30 21:22:44 -0400

    Back to development: 0.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='desfire',
-    version='0.2',
+    version='0.3.dev0',
     description="MIFARE DESFire NFC card communication protocol",
     long_description=readme + '\n\n' + history,
     author="Mikko Ohtamaa",
